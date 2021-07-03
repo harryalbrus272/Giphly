c1a96886beabed52667be853866c5b908781223c
Shashwat Kumar Singh <73544122+harryalbrus272@users.noreply.github.com>
2021-07-03 20:35:49 +0530

    style chenges to the post

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -19,6 +19,7 @@ const Posts = () => {
         quidem vero sequi, aspernatur officiis exercitationem debitis nostrum id
         quos, et reiciendis enim laudantium quia.
         <img
+        style={{margin: '10px auto'}}
           height="200"
           width="400"
           src="https://images.unsplash.com/photo-1593642702821-c8da6771f0c6?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=889&q=80"
